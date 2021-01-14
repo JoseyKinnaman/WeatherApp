66b94ed7cd577000a7743b64ed17d81aae4f9a27
Josey Kinnaman <josey.kinnaman@planetargon.com>
2021-01-14 14:35:56 -0800

    Cleanup undeeded code and components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,12 @@
 import './App.css';
 import CurrentLocation from './CurrentLocation';
-import Location from './Location'
+
 
 const App = () => {
-  // const {location: currentLocation, error: currentError} = useCurrentLocation();
   return (
     <div className="App">
       <h1>Weather App</h1>
       <CurrentLocation />
-      <Location/>
     </div>
   );
 }
