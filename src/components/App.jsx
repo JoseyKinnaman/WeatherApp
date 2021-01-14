89c12be2fcd0d5a18f700252fdd74e4fe725184a
Josey Kinnaman <josey.kinnaman@planetargon.com>
2021-01-14 12:46:07 -0800

    Add style sheets for Location and Weather Compents and app title

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import Location from './Location';
 const App = () => {
   return (
     <div className="App">
+      <h1>Weather App</h1>
       <Location />
     </div>
   );
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,11 @@
 import React, {useEffect} from 'react';
 
 const Weather = () => {
+  return(
+    <div className="">
+      
+    </div>
+  )
 
 }
 
