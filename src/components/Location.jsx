34b6e40e112bb1694d8c85e4522c9714d9850482
Josey Kinnaman <josey.kinnaman@planetargon.com>
2021-01-14 13:04:08 -0800

    Add semantic ui, input box for location, and hook to hold location input

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,10 +1,26 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import Weather from './Weather';
 
 const Location = () => {
+  const [location, setLocation] = useState('Portland');
 return(
-  <div className="">
-    <Weather />
+  <div>
+  <div className="ui form">
+    <div className="field">
+      <label>
+        <input
+          type="text"
+          className="input"
+          value={location}
+          onChange={e => setLocation(e.target.value)}
+        />
+      </label>
+    </div>
+    {location}
+  </div>
+    < Weather
+    location={location}
+    />
   </div>
 )
 }
