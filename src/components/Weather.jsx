963cd8482ff2d720bf5c9c8422a9aa50d4e5b63c
Josey Kinnaman <josey.kinnaman@planetargon.com>
2021-01-14 17:38:31 -0800

    Refactor to add axios api call and loading hook

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,27 +1,37 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Weather = ({location}) => {
-  
+  const [loading, setLoading] = useState(false)
+  const [weather, setWeather] = useState("sunny")
 
   useEffect(() => {
     if(!location){
-      alert('Loading...');
-      // loading hook?
+      setLoading(true);
       return;
       }
       const lat = location[0];
       const lon = location[1];
-    async function getWeather(){
-      const response = await fetch(`api.openweathermap.org/data/2.5/weather?lat=${lat}&${lon}&appid=${process.env.REACT_APP_API_KEY}`)
-      const body = await response.json();
-      console.log(response.json)
+    const getWeather = async () => {
+      const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=82f159dc001a7193a1e47a71b24e4e30`)
+      console.log(data)
+      setLoading(false)
     }
     getWeather()
   },[location])
 
   return(
-    <div className="">
-
+    <div className="ui segment">
+      <h3>Weather by Area</h3>
+      {loading
+      ? <div className="ui active inverted dimmer">
+        <div className="ui text loader">Loading</div>
+      </div>
+      :
+      <div className="">
+        <h4>Hello</h4>
+      </div>
+      }
     </div>
   )
 
